e580e2d30152c122bdd32d7fd2777eed9d8f4759
tropadohest <hudowphe.carneiro@aluno.edu.es.gov.br>
2026-09-16 16:21:31 -0300

    JS.js

--- JS.js
+++ JS.js
@@ -7,16 +7,19 @@
      };
      image.push(img)
    }
-  console.log(image)
+  
   
   startGame();
   function startGame(){
+   var frontFaces = document.getElementsByClassName("front");
     for(var i = 0; i < 20; i++){
       var card = document.querySelector("#card" + i)
       card.style.left = i % 5 === 0 ? 5 + "px" : i % 5 * 200 + 5 + "px";
       card.style.top = Math.floor (i / 5) * 215 + "px";
 
       card.addEventListener("click",flipCard,false);
+
+      frontFaces[i].style.background = "url('"+ image[i].src +"')"
     }
   }
 
